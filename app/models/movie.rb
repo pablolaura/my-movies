b15e0fa1b25ab4a57736c36291cf91f4f605e0c8
ruby 29 <ruby.29@metralha.cec.ime.usp.br>
2017-01-30 22:02:59 -0200

    final com erros

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,16 @@ class Movie < ActiveRecord::Base
 
   belongs_to :director, class_name: "Director", foreign_key: "director_id", optional: true
   has_and_belongs_to_many :actors, association_foreign_key: 'actor_id', join_table: 'actors_movies'
+
+  has_many :classifications
+  has_many :users, through: :classifications
+
+  def average_score
+    classifications.average(:score).to_f
+    if score.nil?
+      return num
+    else
+      return score.to_f
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,7 @@ Rails.application.routes.draw do
   put 'movies/:id' => "movies#update"
 
   root 'movies#index'
+
+  post 'movies/:id/classifications' => "moveis#create_classification"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
